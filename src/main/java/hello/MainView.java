b38f4d959fe11af05f0b0f4cf8b0dcf3563f33a3
Tyvain <tyvain@gmail.com>
2018-12-07 11:26:01 +1100

    Init everyonedraw

--- src/main/java/hello/MainView.java
+++ src/main/java/hello/MainView.java
@@ -1,126 +1,68 @@
 package hello;
 
-import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.upload.Upload;
-import com.vaadin.flow.component.upload.UploadI18N;
-import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
-import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.spring.annotation.UIScope;
-import org.springframework.util.StringUtils;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.List;
 
 @Route
+@StyleSheet("frontend://styles.css")
 public class MainView extends VerticalLayout {
+    List<String> colors = Arrays.asList("w3-flat-turquoise",
+            "w3-flat-emerald",
+            "w3-flat-peter-river",
+            "w3-flat-amethyst",
+            "w3-flat-wet-asphalt",
+            "w3-flat-green-sea",
+            "w3-flat-nephritis",
+            "w3-flat-belize-hole",
+            "w3-flat-wisteria",
+            "w3-flat-midnight-blue",
+            "w3-flat-sun-flower",
+            "w3-flat-carrot",
+            "w3-flat-alizarin",
+            "w3-flat-clouds",
+            "w3-flat-concrete",
+            "w3-flat-orange",
+            "w3-flat-pumpkin",
+            "w3-flat-pomegranate",
+            "w3-flat-silver",
+            "w3-flat-asbestos");
 
-//	private final CustomerRepository repo;
-//
-//	private final CustomerEditor editor;
-//
-//	final Grid<Customer> grid;
-//
-//	final TextField filter;
-//
-//	private final Button addNewBtn;
+    public MainView() {
+        HorizontalLayout topLayout = new HorizontalLayout();
+        colors.stream().forEach(color -> addNewColorButton(topLayout, color));
+        Span main = new Span();
+        for (int i = 0; i < 1600; i++) {
+            if (i % 40 == 0) {
+                add(main);
+                main = new Span();
+            }
+            addButton(main);
+        }
+        add(topLayout, main);
+    }
 
-	public MainView(CustomerRepository repo, CustomerEditor editor) {
-//		this.repo = repo;
-//		this.editor = editor;
-//		this.grid = new Grid<>(Customer.class);
-//		this.filter = new TextField();
-//		this.addNewBtn = new Button("New customer", VaadinIcon.PLUS.create());
-//
-//		// build layout
-//		HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
-//		add(actions, grid, editor);
-//
-//		grid.setHeight("300px");
-//		grid.setColumns("id", "firstName", "lastName");
-//		grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
-//
-//		filter.setPlaceholder("Filter by last name");
-//
-//		// Hook logic to components
-//
-//		// Replace listing with filtered content when user changes filter
-//		filter.setValueChangeMode(ValueChangeMode.EAGER);
-//		filter.addValueChangeListener(e -> listCustomers(e.getValue()));
-//
-//		// Connect selected Customer to editor or hide if none is selected
-//		grid.asSingleSelect().addValueChangeListener(e -> {
-//			editor.editCustomer(e.getValue());
-//		});
-//
-//		// Instantiate and edit new Customer the new button is clicked
-//		addNewBtn.addClickListener(e -> editor.editCustomer(new Customer("", "")));
-//
-//		// Listen changes made by the editor, refresh data from backend
-//		editor.setChangeHandler(() -> {
-//			editor.setVisible(false);
-//			listCustomers(filter.getValue());
-//		});
-//
-//		// Initialize listing
-//		listCustomers(null);
+    private void addNewColorButton(HorizontalLayout topLayout, String color) {
+        Button btn = new Button();
+        btn.addClassName(color);
+        topLayout.add(btn);
+    }
 
-		MemoryBuffer buffer = new MemoryBuffer();
-		Upload upload = new Upload(buffer);
-		upload.setId("i18n-upload");
+    private void addButton(Span main) {
+        Button btn = new Button();
+        btn.addClickListener(event -> click(event));
+        main.add(btn);
+    }
 
-		upload.addSucceededListener(event -> System.out.println("Done!"));
-
-		UploadI18N i18n = new UploadI18N();
-		i18n.setDropFiles(
-				new UploadI18N.DropFiles().setOne("Перетащите файл сюда...")
-						.setMany("Перетащите файлы сюда..."))
-				.setAddFiles(new UploadI18N.AddFiles()
-						.setOne("Выбрать файл").setMany("Добавить файлы"))
-				.setCancel("Отменить")
-				.setError(new UploadI18N.Error()
-						.setTooManyFiles("Слишком много файлов.")
-						.setFileIsTooBig("Слишком большой файл.")
-						.setIncorrectFileType("Некорректный тип файла."))
-				.setUploading(new UploadI18N.Uploading()
-						.setStatus(new UploadI18N.Uploading.Status()
-								.setConnecting("Соединение...")
-								.setStalled("Загрузка застопорилась.")
-								.setProcessing("Обработка файла..."))
-						.setRemainingTime(
-								new UploadI18N.Uploading.RemainingTime()
-										.setPrefix("оставшееся время: ")
-										.setUnknown(
-												"оставшееся время неизвестно"))
-						.setError(new UploadI18N.Uploading.Error()
-								.setServerUnavailable("Сервер недоступен")
-								.setUnexpectedServerError(
-										"Неожиданная ошибка сервера")
-								.setForbidden("Загрузка запрещена")))
-				.setUnits(Stream
-						.of("Б", "Кбайт", "Мбайт", "Гбайт", "Тбайт", "Пбайт",
-								"Эбайт", "Збайт", "Ибайт")
-						.collect(Collectors.toList()));
-
-		upload.setI18n(i18n);
-		add(upload);
-	}
-
-//	// tag::listCustomers[]
-//	void listCustomers(String filterText) {
-//		if (StringUtils.isEmpty(filterText)) {
-//			grid.setItems(repo.findAll());
-//		}
-//		else {
-//			grid.setItems(repo.findByLastNameStartsWithIgnoreCase(filterText));
-//		}
-//	}
-//	// end::listCustomers[]
-
+    private void click(ClickEvent<Button> event) {
+        event.getSource()
+                .addClassName("w3-flat-asbestos");
+    }
 }
