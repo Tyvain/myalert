ef74102ab1504be5e98ad94aaa920b38e2420172
Sylvain AUGER-LEGER <sylvain.auger-leger@unc.nc>
2020-03-04 17:42:49 +1100

    init hello world

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -15,18 +15,18 @@ public class Application {
         SpringApplication.run(Application.class);
     }
 
-    @Bean
-    public CommandLineRunner loadData(PixelRepository repository) {
-        return (args) -> {
-            for (int row = 0; row < SIZE; row++) {
-                for (int col = 0; col < SIZE; col++) {
-                    repository.save(new Pixel(new Point(col, row), "_abebc6"));
-                }
-            }
-            repository.save(new Pixel(new Point(0, 39), "_1abc9c"));
-            repository.save(new Pixel(new Point(0, 0), "_af7ac5"));
-            repository.save(new Pixel(new Point(39, 0), "_212f3d"));
-            repository.save(new Pixel(new Point(39, 39), "_922b21"));
-        };
-    }
+//    @Bean
+//    public CommandLineRunner loadData(PixelRepository repository) {
+//        return (args) -> {
+//            for (int row = 0; row < SIZE; row++) {
+//                for (int col = 0; col < SIZE; col++) {
+//                    repository.save(new Pixel(new Point(col, row), "_abebc6"));
+//                }
+//            }
+//            repository.save(new Pixel(new Point(0, 39), "_1abc9c"));
+//            repository.save(new Pixel(new Point(0, 0), "_af7ac5"));
+//            repository.save(new Pixel(new Point(39, 0), "_212f3d"));
+//            repository.save(new Pixel(new Point(39, 39), "_922b21"));
+//        };
+//    }
 }
