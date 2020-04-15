394b6b622e83cc002118e02601aeb35cc42813ce
unknown <tyvain@gmail.com>
2020-04-15 12:49:16 +1100

    trest CI

---
+++ src/main/java/nc/unc/View1.java
@@ -0,0 +1,8 @@
+package nc.unc;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "view1")
+public class View1 extends VerticalLayout {
+}
